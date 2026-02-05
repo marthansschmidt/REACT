82d9b204c6f2a6250705605f25ab932dffb498d3
Mac <mac@Macs-MacBook-Air.local>
2026-02-05 15:54:53 +0200

    Ülesanne 1 tehtud

--- React projekt/src/App.jsx
+++ React projekt/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem.js';
+import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
-  return(
-    <div className="App">
-      <ExpenseItem />
-    </div>
-  )
-}
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New book',
+      price: 30.99,
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New jeans',
+      price: 99.99,
+    },
+  ];
 
- 
-export default App
+  return (
+    <div className="App">
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
+    </div>
+  );
+};
+
+export default App;
--- React projekt/src/components/ExpenseDate.jsx
+++ React projekt/src/components/ExpenseDate.jsx
@@ -1,8 +1,14 @@
-import "./ExpenseDate.css";
+import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString('en-US', {
+    day: '2-digit',
+  });
+
+  const month = props.date.toLocaleString('en-US', {
+    month: 'long',
+  });
+
   const year = props.date.getFullYear();
 
   return (
